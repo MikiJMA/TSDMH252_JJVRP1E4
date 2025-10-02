17126bca8656c367aba40abc4fa06f497a375cc3
JulianVR <julianvillegas191@gmail.com>
2025-10-02 00:14:57 -0600

    Initial commit

---
+++ app/src/main/java/com/example/tsdmh252_jjvrp1e4/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.tsdmh252_jjvrp1e4;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn1, btn2;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+    }
+
+    public void oneclickbtn (View v) {
+        Intent intent = new Intent(this, PP1Activity.class);
+        startActivity(intent);
+    }
+
+    public void oneclickbtn2 (View v) {
+        Intent intent = new Intent(this, PP2Activity.class);
+        startActivity(intent);
+    }
+}
